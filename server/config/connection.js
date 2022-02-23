4316cc1adeae8356e3118945ba9dccc70cd29dbb
AdamDagi <mugutdinovshiha02@gmail.com>
2022-02-23 11:20:38 -0500

    fixed server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://AdamDagi:Shiha005@cluster0.j80q4.mongodb.net/book-search-app?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
