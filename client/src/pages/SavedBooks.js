fd578ae427b60b03423c6fd63384d493a6f62583
AdamDagi <mugutdinovshiha02@gmail.com>
2022-03-04 15:08:11 -0500

    added saved books function

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -20,7 +20,14 @@ const SavedBooks = () => {
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
-
+  useEffect(() => {
+    setUserData({});
+  }, []);
+  useEffect(() => {
+    if (data) {
+      setUserData(data.me);
+    }
+  }, [data]);
   useEffect(() => {
     const getUserData = async () => {
       try {
@@ -84,12 +91,12 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
+          {userData.savedBooks && userData.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks && userData.savedBooks.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
                 {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
