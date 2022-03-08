2ffa6225549f6c91cdc4b01d5ca314d27d899b16
AdamDagi <mugutdinovshiha02@gmail.com>
2022-03-08 11:34:13 -0500

    added readme.md

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://AdamDagi:Shiha005@cluster0.j80q4.mongodb.net/book-search-app?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
